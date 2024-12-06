de60be5bf917d9073a56f51cebf67700357d649f
Ayu Sudi <ayusudi.abc@gmail.com>
2024-12-06 22:17:02 +0700

    doc: adding screenshots and ready to build

--- README.md
+++ README.md
@@ -2,5 +2,16 @@
 
 This project for test at CloudFit.
 
-name : Ayu Sudi Dwijayanti
+name : Ayu Sudi Dwijayanti  
 email : ayusudi.abc@gmail.com
+
+> I am using React Navigation with Stack, React Native Reanimated, and Expo Linear Gradient. I started this project after lunch at 13:00 (Jakarta Time) and finished it by 21:00. While I considered implementing a better transition between the home page and the detail page, I prioritized time constraints and focused on completing the project and creating content that I love. Overall, I enjoyed working on this homework project. Thank you for the opportunity!
+
+Here is the page :
+
+- Home Page & Slider Carousel  
+  ![Home Page](./screenshot/homepage.png)
+  ![Slider](./screenshot/homepage-slide.png)
+
+- Detail Page  
+  ![Detail Page](./screenshot/detailpage.png)
--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -1,18 +1,24 @@
 import { LinearGradient } from "expo-linear-gradient";
 import { StyleSheet, View, Text } from "react-native";
-import Animated, { useAnimatedScrollHandler, useSharedValue } from "react-native-reanimated";
+import Animated, {
+  useAnimatedScrollHandler,
+  useSharedValue,
+} from "react-native-reanimated";
 import data from "../data";
 import SliderCard from "../components/SliderCard";
 
 export default function Slider() {
   const scrollX = useSharedValue(0);
   const onScrollHandler = useAnimatedScrollHandler({
-    onScroll: (e) => (scrollX.value = e.contentOffset.x)
+    onScroll: (e) => (scrollX.value = e.contentOffset.x),
   });
 
   return (
     <View style={styles.container}>
-      <LinearGradient style={styles.linearGradient} colors={["#170B15", "#1c1238", "#6A4C82"]}>
+      <LinearGradient
+        style={styles.linearGradient}
+        colors={["#170B15", "#1c1238", "#6A4C82"]}
+      >
         <Text style={styles.header}>My Gig Journal</Text>
         <Animated.FlatList
           data={data}
@@ -37,7 +43,7 @@ const styles = StyleSheet.create({
     textAlign: "center",
     fontWeight: "bold",
     fontSize: 40,
-    top: 100,
+    top: 90,
     position: "absolute",
     color: "white",
   },
